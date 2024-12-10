52e01e9da522263bd460b147afe2d6a8969368ce
Dave Hughes <648035+dlh3@users.noreply.github.com>
2024-12-10 15:07:26 -0800

    Manifest changes and rename the content script

---
+++ public/apple.js
@@ -0,0 +1,2 @@
+const logStyle = "font-size: 18px; font-weight: 1000;";
+console.info("%cLoading Newton's %cApple 🍏", `${logStyle} color: rgb(47, 225, 185);`, `${logStyle} color: rgb(248, 72, 116);`);
